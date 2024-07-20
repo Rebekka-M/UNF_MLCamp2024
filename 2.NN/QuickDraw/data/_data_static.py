ae9af16fc7b936ba95027a404f120c1968a0841a
rebekka-m <rebekka.whm@gmail.com>
2024-07-20 11:52:58 +0200

    model architecture removed

--- 2.NN/QuickDraw/data/_data_static.py
+++ 2.NN/QuickDraw/data/_data_static.py
@@ -1,11 +1,13 @@
 dsize = (28, 28)
 TEGNINGER = [
     "apple", "banana", "birthday cake",
-    "blueberry", "bread", "broccoli",
-    "carrot", "cookie", "coffee cup",
-    "donut", "fish", "frying pan",
-    "grapes", "hot dog", "ice cream",
+    "bread", "broccoli", "carrot",
+    "cookie", "coffee cup", "donut", 
+    "eye", "fish", "frying pan", "guitar",
+    "headphones", "hot dog", "ice cream",
     "lobster", "lollipop", "mushroom",
     "onion", "pear", "peas", "pineapple",
-    "pizza", "strawberry", "watermelon"
+    "pizza", "snowflake", "strawberry",
+    "squiggle", "sock", "tooth",
+    "t-shirt", "umbrella", "watermelon"
 ]
--- 2.NN/QuickDraw/model.py
+++ 2.NN/QuickDraw/model.py
@@ -1,15 +1,12 @@
 """Modul til definering af netværksarkitektur"""
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from data._data_static import TEGNINGER
-from get_data import get_dataset
-from options import Hyperparameters, name_generator
-from train import train
-
-import numpy as np
+from options import Hyperparameters
 
 C = len(TEGNINGER)
 
@@ -37,18 +34,8 @@ class Net(nn.Module):
         self.criterion = nn.CrossEntropyLoss()
         setattr(self.hyperparameters, 'loss', self.criterion.__class__.__name__)
 
-        input_size = 28
-        num_classes = C
-        #self.num_classes = num_classes
-        self.conv1 = nn.Sequential(nn.Conv2d(1, 32, 5, bias=False), nn.ReLU(inplace=True), nn.MaxPool2d(2,2))
-        self.conv2 = nn.Sequential(nn.Conv2d(32, 64, 5, bias=False), nn.ReLU(inplace=True), nn.MaxPool2d(2, 2))
-        dimension = int(64 * pow(input_size/4 - 3, 2))
-        self.fc1 = nn.Sequential(nn.Linear(dimension, 512), nn.Dropout(0.5))
-        self.fc2 = nn.Sequential(nn.Linear(512, 128), nn.Dropout(0.5))
-        self.fc3 = nn.Sequential(nn.Linear(128, num_classes))
-
         # Definer lagene i netværket
-        #raise NotImplementedError("Implementer Netværksarkitektur. Det sidste lag er defineret nedenunder")
+        raise NotImplementedError("Implementer Netværksarkitektur. Det sidste lag er defineret nedenunder")
 
     def forward(self, x: torch.Tensor):
         """
@@ -60,15 +47,7 @@ class Net(nn.Module):
         Returns:
         torch.Tensor: Output tensor
         """
-        x = x.reshape(-1, 1, 28, 28)
-        output = self.conv1(x)
-        output = self.conv2(output)
-        output = output.view(output.size(0), -1)
-        output = self.fc1(output)
-        output = self.fc2(output)
-        output = self.fc3(output)
-        
-        #raise NotImplementedError("Implementer forward pass")
+        raise NotImplementedError("Implementer forward pass")
 
         return output
     
